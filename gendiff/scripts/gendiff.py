2eaaccacf618fc0d0b92174a0aa5bf8bf339aafa
Alex Iset <fl3shtrader@yandex.ru>
2025-01-17 17:03:10 +0300

    added: __init__.py

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python3
 import argparse
 
+def main():
+    parser = argparse.ArgumentParser(
+        usage='%(prog)s [-h] [-f FORMAT] first_file second_file',
+        description='Compares two configuration files and shows a difference.',
+    )
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+    parser.add_argument('-f', '--format', help='set format of output')
+    parser.parse_args()
 
-parser = argparse.ArgumentParser(
-    usage='%(prog)s [-h]',
-    description='Compares two configuration files and shows a difference.',
-)
-parser.add_argument('first_file')
-parser.add_argument('second_file')
-args = parser.parse_args()
-parser.print_help()
+
+if __name__ == "__main__":
+    main()
