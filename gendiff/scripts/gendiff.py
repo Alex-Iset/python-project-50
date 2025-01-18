761e473f1137d12db2a552d3a55c260369aa8f7f
Alex Iset <fl3shtrader@yandex.ru>
2025-01-18 11:37:06 +0300

    fixed

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,15 +1,9 @@
 #!/usr/bin/env python3
-import argparse
+from gendiff.parser import read_and_pars
+
 
 def main():
-    parser = argparse.ArgumentParser(
-        usage='%(prog)s [-h] [-f FORMAT] first_file second_file',
-        description='Compares two configuration files and shows a difference.',
-    )
-    parser.add_argument('first_file')
-    parser.add_argument('second_file')
-    parser.add_argument('-f', '--format', help='set format of output')
-    parser.parse_args()
+    read_and_pars()
 
 
 if __name__ == "__main__":
