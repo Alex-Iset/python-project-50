92fc4ac1d64faf3668d36767d88a93fd74e662b0
Alex Iset <fl3shtrader@yandex.ru>
2025-01-20 21:19:46 +0300

    fixed

--- Makefile
+++ Makefile
@@ -5,11 +5,11 @@ run-gd-help:
 	uv run gendiff -h
 
 run-gd:
-	uv run gendiff -d gendiff/json_files/file1.json gendiff/json_files/file2.json
+	uv run gendiff -d tests/test_data/file1.json tests/test_data/file2.json
 
-read-gd-files:
-	uv run gendiff --file gendiff/json_files/file1.json
-	uv run gendiff --file gendiff/json_files/file2.json
+read-files:
+	uv run gendiff --file tests/test_data/file1.json
+	uv run gendiff --file tests/test_data/file2.json
 
 test:
 	uv run pytest
---
+++ tests/test_gendiff.py
@@ -0,0 +1,31 @@
+import os
+
+import json
+
+from gendiff import diff
+
+
+def get_test_data_path(file_name):
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    return os.path.join(current_dir, 'test_data', file_name)
+
+
+def read(file_path):
+    with open(file_path) as f:
+        return json.load(f)
+
+
+file1 = read(get_test_data_path('file1.json'))
+file2 = read(get_test_data_path('file2.json'))
+result = '''{
+- follow: False
+  host: hexlet.io
+- proxy: 123.234.53.22
+- timeout: 50
++ timeout: 20
++ verbose: True
+}'''
+
+
+def test_gendiff():
+    assert diff(file1, file2) == result
