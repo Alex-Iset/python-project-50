2332abcab557cbe79f1d6b60e7da3630e8698103
Alex Iset <fl3shtrader@yandex.ru>
2025-01-27 09:40:26 +0300

    added: new arg

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -5,7 +5,7 @@ from gendiff.parser import parse_args
 
 def main():
     args = parse_args()
-    print(generate_diff(args.first_file, args.second_file))
+    print(generate_diff(args.first_file, args.second_file, args.format))
 
 
 if __name__ == "__main__":
