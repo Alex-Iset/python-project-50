0823745e67a519a5173661b0da951735ac11b1ee
Alex Iset <fl3shtrader@yandex.ru>
2025-01-28 18:50:55 +0300

    added: test for json-format

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -10,19 +10,23 @@ def get_test_data_path(file_name):
 
 
 file1 = get_test_data_path('file1.json')  # нужно будет переделать
-file2 = get_test_data_path('file2.json')  # нужно будет переделать
-file3 = get_test_data_path('file1.yaml')  # нужно будет переделать
-file4 = get_test_data_path('file2.yaml')  # нужно будет переделать
+file2 = get_test_data_path('file2.json')
+file3 = get_test_data_path('file1.yaml')
+file4 = get_test_data_path('file2.yaml')
 
-
-def test_gendiff():
+def test_gendiff(): # нужно будет переделать
     stylish = load_supp_form_file(
-        get_test_data_path('stylish_json_yaml.txt')
+        get_test_data_path('stylish_output.txt')
     )
     plain = load_supp_form_file(
-        get_test_data_path('plain_json_yaml.txt')
+        get_test_data_path('plain_output.txt')
+    )
+    json_format = load_supp_form_file(
+        get_test_data_path('json_format_output.txt')
     )
     assert generate_diff(file1, file2, "stylish") == stylish
     assert generate_diff(file3, file4, "stylish") == stylish
     assert generate_diff(file1, file2, "plain") == plain
     assert generate_diff(file3, file4, "plain") == plain
+    assert generate_diff(file1, file2, "json") == json_format
+    assert generate_diff(file3, file4, "json") == json_format
