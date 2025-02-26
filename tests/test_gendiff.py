0a2e46ee6b78ca306ba3cc8d6ce8f279896172f8
Alex Iset <fl3shtrader@yandex.ru>
2025-02-27 00:05:07 +0300

    added: fixture_loader

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -3,7 +3,6 @@ from pathlib import Path
 import pytest
 
 from gendiff.generate_diff import generate_diff
-from gendiff.parser import file_loader
 
 
 def get_path(fixture_name):
@@ -11,55 +10,60 @@ def get_path(fixture_name):
     return base_dir / fixture_name
 
 
+def fixture_loader(path):
+    with open(path) as f:
+        return f.read()
+
+
 @pytest.mark.parametrize(
-    "file1, file2, output, formatter",
+    'file1, file2, output, formatter',
     [
         (
             # stylish (json)
-            get_path("file1.json"),
-            get_path("file2.json"),
-            get_path("stylish_output.txt"),
-            "stylish"
+            get_path('file1.json'),
+            get_path('file2.json'),
+            get_path('stylish_output.txt'),
+            'stylish'
         ),
         (
             # stylish (yaml)
-            get_path("file1.yaml"),
-            get_path("file2.yaml"),
-            get_path("stylish_output.txt"),
-            "stylish"
+            get_path('file1.yaml'),
+            get_path('file2.yaml'),
+            get_path('stylish_output.txt'),
+            'stylish'
         ),
         (
             # plain (json)
-            get_path("file1.json"),
-            get_path("file2.json"),
-            get_path("plain_output.txt"),
-            "plain"
+            get_path('file1.json'),
+            get_path('file2.json'),
+            get_path('plain_output.txt'),
+            'plain'
         ),
         (
             # plain (yaml)
-            get_path("file1.yaml"),
-            get_path("file2.yaml"),
-            get_path("plain_output.txt"),
-            "plain"
+            get_path('file1.yaml'),
+            get_path('file2.yaml'),
+            get_path('plain_output.txt'),
+            'plain'
         ),
         (
             # json_format (json)
-            get_path("file1.json"),
-            get_path("file2.json"),
-            get_path("json_format_output.txt"),
-            "json"
+            get_path('file1.json'),
+            get_path('file2.json'),
+            get_path('json_format_output.txt'),
+            'json'
         ),
         (
             # json_format (yaml)
-            get_path("file1.yaml"),
-            get_path("file2.yaml"),
-            get_path("json_format_output.txt"),
-            "json"
+            get_path('file1.yaml'),
+            get_path('file2.yaml'),
+            get_path('json_format_output.txt'),
+            'json'
         )
     ]
 )
 def test_gendiff(file1, file2, output, formatter):
-    load_output_file = file_loader(output)
+    load_output_file = fixture_loader(output)
     assert generate_diff(
         file1, file2, formatter=formatter
     ) == load_output_file
