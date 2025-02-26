f9403b2fb164026db3b4d1d8c3bb7ce9e6be5bd0
Alex Iset <fl3shtrader@yandex.ru>
2025-02-26 23:40:55 +0300

    added: constant

--- gendiff/formatters/__init__.py
+++ gendiff/formatters/__init__.py
@@ -1,13 +1,14 @@
-from gendiff.formatters.json_format import json_format
+from gendiff.consts import FORMATS
+from gendiff.formatters.json import json_format
 from gendiff.formatters.plain import plain
 from gendiff.formatters.stylish import stylish
 
 
 def get_formatter(diff, formatter):
     match formatter:
-        case "stylish":
+        case FORMATS.STYLISH:
             return stylish(diff)
-        case "plain":
+        case FORMATS.PLAIN:
             return plain(diff)
-        case "json":
+        case FORMATS.JSON:
             return json_format(diff)
