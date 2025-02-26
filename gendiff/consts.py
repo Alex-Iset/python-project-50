15c731902afa0b28054499377c2514284bdf8fbe
Alex Iset <fl3shtrader@yandex.ru>
2025-02-26 23:47:49 +0300

    added: constants

--- gendiff/consts.py
+++ gendiff/consts.py
@@ -1,4 +1,18 @@
-STATUS = "status"
-VALUE = "value"
-OLD_VALUE = "old_value"
-NEW_VALUE = "new_value"
+from collections import namedtuple
+
+STATUS = 'status'
+VALUE = 'value'
+OLD_VALUE = 'old_value'
+NEW_VALUE = 'new_value'
+
+_FORMAT_VALUES = ('stylish', 'plain', 'json')
+FORMATS = namedtuple(
+    'FormatChoices', map(lambda x: x.upper(), _FORMAT_VALUES)
+)(*_FORMAT_VALUES)
+
+_TYPE_VALUES = ('removed', 'added', 'nested', 'updated', 'unchanged')
+STATUSES = namedtuple(
+    'FormatTypes', map(lambda x: x.upper(), _TYPE_VALUES)
+)(*_TYPE_VALUES)
+
+INDENT = '    '
