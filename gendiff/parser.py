1f4f83c2c9f1fdf95768d8e3e63677bdb20270f2
Alex Iset <fl3shtrader@yandex.ru>
2025-02-26 23:05:24 +0300

    fixed: reading format files .txt, moved to tests

--- gendiff/parser.py
+++ gendiff/parser.py
@@ -1,16 +1,12 @@
 import json
-import os
 
 import yaml
 
 
-def file_loader(path):
-    extension = os.path.splitext(path)[1]
+def file_loader(path, extension):
     with open(path) as f:
         match extension:
-            case ".json":
+            case '.json':
                 return json.load(f)
-            case ".yml" | ".yaml":
+            case '.yml' | '.yaml':
                 return yaml.safe_load(f)
-            case ".txt":
-                return f.read()
